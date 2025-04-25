eda90d2266189449c477b9a9ac455fe36948b871
timuravlakulov <avlyakulovtimur@gmail.com>
2025-04-25 08:31:27 +0200

    add main page and not found page

--- task-tracker-ui/src/app/app-routing.module.ts
+++ task-tracker-ui/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from './auth/login/login.component';
 import {RegisterComponent} from './auth/register/register.component';
-import {AppComponent} from './app.component';
+import {MainPageComponent} from './blocks/main-page/main-page.component';
+import {NotFoundComponent} from './blocks/not-found/not-found.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent},
+  { path: '', component: MainPageComponent},
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
+  { path: '**', component: NotFoundComponent},
 ];
 
 @NgModule({
--- task-tracker-ui/src/app/blocks/blocks.module.ts
+++ task-tracker-ui/src/app/blocks/blocks.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import {RouterLink} from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MainPageComponent,
+    NotFoundComponent
   ],
   imports: [
     CommonModule,
-    RouterLink
+    RouterLink,
+    NgOptimizedImage
   ],
   exports: [
     HeaderComponent,
