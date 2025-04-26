e050d8b44979596a1e12992ea9c062b121e48cb5
timuravlakulov <avlyakulovtimur@gmail.com>
2025-04-26 14:16:29 +0200

    finish with not found component

--- task-tracker-ui/src/app/auth/login/login.component.ts
+++ task-tracker-ui/src/app/auth/login/login.component.ts
@@ -1,7 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {AuthService} from '../../services/auth.service';
-import {timeout, TimeoutError} from 'rxjs';
 
 @Component({
   selector: 'app-login',
