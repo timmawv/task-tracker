2cc44c639ea181cb1401e7055c4ec711965812df
timuravlakulov <avlyakulovtimur@gmail.com>
2025-04-28 18:01:30 +0200

    add filtering for state of task

--- task-tracker-ui/src/app/tasks/tasks-page/tasks-page.component.ts
+++ task-tracker-ui/src/app/tasks/tasks-page/tasks-page.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {TaskService} from '../../services/task.service';
-import {Observable} from 'rxjs';
+import {map, Observable} from 'rxjs';
 import {Task} from '../../../shared/models/Task';
+import {TaskState} from '../../../shared/models/TaskState';
 
 @Component({
   selector: 'app-tasks-page',
@@ -12,11 +13,26 @@ import {Task} from '../../../shared/models/Task';
 export class TasksPageComponent implements OnInit {
 
   tasks$!: Observable<Task[]>;
+  notStartedTasks$!: Observable<Task[]>;
+  inProgressTasks$!: Observable<Task[]>;
+  finishedTasks$!: Observable<Task[]>;
 
   constructor(private taskService: TaskService) {
   }
 
   ngOnInit() {
     this.tasks$ = this.taskService.getTasks();
+
+    this.notStartedTasks$ = this.tasks$.pipe(
+      map(tasks => tasks.filter(task => task.taskState === TaskState.NOT_STARTED))
+    );
+
+    this.inProgressTasks$ = this.tasks$.pipe(
+      map(tasks => tasks.filter(task => task.taskState === TaskState.IN_PROGRESS))
+    );
+
+    this.finishedTasks$ = this.tasks$.pipe(
+      map(tasks => tasks.filter(task => TaskState.FINISHED))
+    );
   }
 }
