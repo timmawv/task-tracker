864dd1aa5276d32ddc8473fa052c8f4ab03ce579
timuravlakulov <avlyakulovtimur@gmail.com>
2025-05-02 09:07:15 +0200

    add task title description constraints

--- task-tracker-api/src/main/java/avlyakulov/timur/taskTrackerApi/dto/TaskDto.java
+++ task-tracker-api/src/main/java/avlyakulov/timur/taskTrackerApi/dto/TaskDto.java
@@ -2,8 +2,10 @@ package avlyakulov.timur.taskTrackerApi.dto;
 
 import avlyakulov.timur.taskTrackerApi.util.TaskState;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,8 +23,10 @@ public class TaskDto {
     private String id;
 
     @NotBlank(message = "Field title can't be null or empty")
+    @Size(max = 30, message = "Max size of the title 30 symbols")
     private String title;
 
+    @Size(max = 100, message = "Max size of the description is 100 symbols")
     private String description;
 
     private TaskState taskState;
