87b9d70c18f9bb676a664ea87cb4798571e79487
timuravlakulov <avlyakulovtimur@gmail.com>
2025-05-03 11:20:05 +0200

    add kafka config

---
+++ task-tracker-api/src/main/java/avlyakulov/timur/taskTrackerApi/config/KafkaConfig.java
@@ -0,0 +1,35 @@
+package avlyakulov.timur.taskTrackerApi.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaConfig {
+
+    @Value("${spring.kafka.producer.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.producer.key-serializer}")
+    private String keySerializer;
+
+    @Value("${spring.kafka.producer.value-serializer}")
+    private String valueSerializer;
+
+    @Value("${spring.kafka.producer.acks}")
+    private String acks;
+
+    @Value("${spring.kafka.producer.properties.delivery.timeout.ms}")
+    private String deliveryTimeout;
+
+    @Value("${spring.kafka.producer.properties.linger.ms}")
+    private String linger;
+
+    @Value("${spring.kafka.producer.properties.request.timeout.ms}")
+    private String requestTimeout;
+
+    @Value("${spring.kafka.producer.properties.enable.idempotence}")
+    private String enableIdempotence;
+
+    @Value("${spring.kafka.producer.properties.max.in.flight.requests.per.connections}")
+    private String maxInFlightRequest;
+}
