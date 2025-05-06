fdf2f9a1d51d2f7e448630c41f9355dfe93839d6
timuravlakulov <avlyakulovtimur@gmail.com>
2025-05-06 08:23:59 +0200

    add kafka configuration

--- task-tracker-api/src/main/java/avlyakulov/timur/taskTrackerApi/config/KafkaConfig.java
+++ task-tracker-api/src/main/java/avlyakulov/timur/taskTrackerApi/config/KafkaConfig.java
@@ -1,7 +1,18 @@
 package avlyakulov.timur.taskTrackerApi.config;
 
+import avlyakulov.timur.taskTrackerApi.dto.TaskDto;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
@@ -32,4 +43,45 @@ public class KafkaConfig {
 
     @Value("${spring.kafka.producer.properties.max.in.flight.requests.per.connections}")
     private String maxInFlightRequest;
+
+    @Value("${spring.kafka.producer.topic}")
+    private String emailTopic;
+
+    Map<String, Object> producerConfig() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
+        config.put(ProducerConfig.ACKS_CONFIG, acks);
+        config.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, deliveryTimeout);
+        config.put(ProducerConfig.LINGER_MS_CONFIG, linger);
+        config.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, requestTimeout);
+        config.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, enableIdempotence);
+        config.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, maxInFlightRequest);
+        config.put(ProducerConfig.RETRIES_CONFIG, Integer.MAX_VALUE);
+
+        return config;
+    }
+
+    @Bean
+    ProducerFactory<String, TaskDto> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfig());
+    }
+
+    @Bean
+    KafkaTemplate<String, TaskDto> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    NewTopic createTopic() {
+        return TopicBuilder.name(emailTopic)
+                .partitions(3)
+                .replicas(3)
+                .configs(Map.of("min.insync.replicas", "2"))//минимум 2 сервера должны быть в синхроне
+                // либо будет exception. Если хоть 1 упадет, то все будут ошибки
+                //тут как раз и ставиться на 1 реплику меньше что если 1 упадет, то сервер будет жить
+                .build();
+    }
 }
