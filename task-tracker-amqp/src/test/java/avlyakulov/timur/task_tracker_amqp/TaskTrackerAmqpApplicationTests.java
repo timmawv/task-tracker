b8094dce398b2cfe47642e90139c0ccd26095aea
timuravlakulov <avlyakulovtimur@gmail.com>
2025-05-08 09:12:56 +0200

    init project

---
+++ task-tracker-amqp/src/test/java/avlyakulov/timur/task_tracker_amqp/TaskTrackerAmqpApplicationTests.java
@@ -0,0 +1,13 @@
+package avlyakulov.timur.task_tracker_amqp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerAmqpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
