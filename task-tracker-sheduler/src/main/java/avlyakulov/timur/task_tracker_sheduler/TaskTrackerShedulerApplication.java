aab94c1d4f01c7522b4eef7d4a7b47cdf1cccce7
timuravlakulov <avlyakulovtimur@gmail.com>
2025-05-12 08:53:10 +0200

    init module scheduler

---
+++ task-tracker-sheduler/src/main/java/avlyakulov/timur/task_tracker_sheduler/TaskTrackerShedulerApplication.java
@@ -0,0 +1,13 @@
+package avlyakulov.timur.task_tracker_sheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerShedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackerShedulerApplication.class, args);
+	}
+
+}
---
+++ task-tracker-sheduler/src/test/java/avlyakulov/timur/task_tracker_sheduler/TaskTrackerShedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package avlyakulov.timur.task_tracker_sheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerShedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
